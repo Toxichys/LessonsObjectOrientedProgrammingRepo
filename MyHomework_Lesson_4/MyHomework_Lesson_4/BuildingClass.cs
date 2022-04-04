4505f287d5e8ca075ee2b9b6a07731a390221324
Toxichys <ToxichSpam@bk.ru>
2022-04-04 11:07:04 +0200

    Урок 4

---
+++ MyHomework_Lesson_4/MyHomework_Lesson_4/BuildingClass.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyHomework_Lesson_4
+{
+    class BuildingClass
+    {
+        /// <summary>
+        /// Статическое поле, которое хранит последний использованный номер здания
+        /// </summary>
+        static int _counter = 0;
+        /// <summary>
+        /// Уникальный номер здания
+        /// </summary>
+        private int _number;
+        /// <summary>
+        /// Высота здания
+        /// </summary>
+        private float _height;
+        /// <summary>
+        /// Количество этажей
+        /// </summary>
+        private int _countFloor;
+        /// <summary>
+        /// Количество квартир
+        /// </summary>
+        private int _countFlat;
+        /// <summary>
+        /// Количество подъездов
+        /// </summary>
+        private int _countEntrance;
+
+        public int Number { get => _number; }
+        public float Height { get => _height; set => _height = value; }
+        public int CountFloor { get => _countFloor; set => _countFloor = value; }
+        public int CountFlat { get => _countFlat; set => _countFlat = value; }
+        public int CountEntrance { get => _countEntrance; set => _countEntrance = value; }
+
+        /// <summary>
+        /// Метод, который увеличивает значение статическое поля использованного номер здания
+        /// и присвоение номара здания для объекта
+        /// </summary>
+        private void NewNumber()
+        {
+            _counter++;
+            _number = _counter;
+        }
+
+        public BuildingClass()
+        {
+            NewNumber();
+        }
+
+        //Добавить методы вычисления высоты этажа, количества квартир в подъезде, количества квартир на этаже
+        /// <summary>
+        /// Получаем высоту этаж
+        /// </summary>
+        /// <returns></returns>
+        public float GetHeightFloor()
+        {
+            return _height / _countFloor;
+        }
+
+        /// <summary>
+        /// Получаем количество квартир в подъезде (считаем, что количество квартир в подъездах одинаковое)
+        /// </summary>
+        /// <returns></returns>
+        public int GetFlatInEntrance()
+        {
+            return _countFlat / _countEntrance;
+        }
+
+        /// <summary>
+        /// Получаем количество квартир на этаже (считаем, что количество квартир на этажах одинаковое)
+        /// </summary>
+        /// <returns></returns>
+        public int GetFlatInFloor()
+        {
+            return GetFlatInEntrance() / _countFloor;
+        }
+    }
+}
---
+++ MyHomework_Lesson_4/MyHomework_Lesson_4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace MyHomework_Lesson_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Реализовать класс для описания здания (уникальный номер здания, высота, этажность, количество квартир, подъездов).
+             * Поля сделать закрытыми, предусмотреть методы для заполнения полей и получения значений полей для печати.
+             * Добавить методы вычисления высоты этажа, количества квартир в подъезде, количества квартир на этаже и т.д.
+             * Предусмотреть возможность, чтобы уникальный номер здания генерировался программно.
+             * Для этого в классе предусмотреть статическое поле, которое бы хранило последний использованный номер здания,
+             * и предусмотреть метод, который увеличивал бы значение этого поля.
+             */
+            BuildingClass building = new BuildingClass();
+            building.Height = 30;
+            building.CountFloor = 9;
+            building.CountFlat = 108;
+            building.CountEntrance = 3;
+            Console.WriteLine($"Здание номе {building.Number}, высота этажей {building.GetHeightFloor()}, кол-во квартир в подъезде {building.GetFlatInEntrance()}, кол-во квартир на этаже {building.GetFlatInFloor()}");
+
+            BuildingClass buildingTwo = new BuildingClass();
+            Console.WriteLine($"Следующий дом с номером {buildingTwo.Number}");
+        }
+    }
+}
