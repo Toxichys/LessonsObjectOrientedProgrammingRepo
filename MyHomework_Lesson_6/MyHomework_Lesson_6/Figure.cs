7f9f8ad02ddb06ab56e021627d1f72218d28658d
Toxichys <ToxichSpam@bk.ru>
2022-04-11 17:40:08 +0200

    Урок 6

---
+++ MyHomework_Lesson_6/MyHomework_Lesson_6/Figure.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyHomework_Lesson_6
+{
+    class Figure
+    {
+        public int _positionX;
+        public int _positionY;
+        public FigureColourEnum _colour;
+        public bool _isVisible;
+        public Figure(int positionX, int positionY, FigureColourEnum colour, bool visible)
+        {
+            _positionX = positionX;
+            _positionY = positionY;
+            _colour = colour;
+            _isVisible = visible;
+        }
+        public void MoveX(int x)
+        {
+            _positionX += x;
+        }
+        public void MoveY(int y)
+        {
+            _positionY += y;
+        }
+        public void ChangeColour(FigureColourEnum colour)
+        {
+            _colour = colour;
+        }
+        public bool IsVisible()
+        {
+            return _isVisible;
+        }
+        public virtual void PrintParameters()
+        {
+            Console.WriteLine($"Значение позиции X {_positionX}  Y {_positionY}");
+            Console.WriteLine($"Значение цвета {_colour}");
+            Console.WriteLine($"Значение видимости {_isVisible}");
+        }
+    }
+    public enum FigureColourEnum
+    {
+        red,
+        green,
+        blue
+    }
+    class Point : Figure
+    {
+        public Point(int positionX, int positionY, FigureColourEnum colour, bool visible) : base(positionX, positionY, colour, visible)
+        {
+        }
+    }
+    class Circle : Point
+    {
+        public double _radius;
+        public Circle(int positionX, int positionY, FigureColourEnum colour, bool visible, double radius) : base(positionX, positionY, colour, visible)
+        {
+            _radius = radius;
+        }
+        public double Square()
+        {
+            return Math.PI * Math.Pow(_radius, 2);
+        }
+        public override void PrintParameters()
+        {
+            base.PrintParameters();
+            Console.WriteLine($"Значение радиуса {_radius}");
+        }
+    }
+    class Rectangle : Point
+    {
+        public double _height;
+        public double _width;
+        public Rectangle(int positionX, int positionY, FigureColourEnum colour, bool visible, double height, double width) : base(positionX, positionY, colour, visible)
+        {
+            _height = height;
+            _width = width;
+        }
+        public double Square()
+        {
+            return _height * _width;
+        }
+        public override void PrintParameters()
+        {
+            base.PrintParameters();
+            Console.WriteLine($"Значение высоты {_height}");
+            Console.WriteLine($"Значение ширины {_width}");
+        }
+    }
+}
---
+++ MyHomework_Lesson_6/MyHomework_Lesson_6/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace MyHomework_Lesson_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Для класса банковский счет переопределить операторы == и != для сравнения информации в двух счетах.
+             * Переопределить метод Equals аналогично оператору ==, не забыть переопределить метод GetHashCode().
+             * Переопределить методToString() для печати информации о счете.
+             * Протестировать функционирование переопределенных методов и операторов на простом примере.
+             */
+            Console.WriteLine("Задание 1");
+            BankAccount bankAccount1 = new BankAccount(1500, BankAccount.TypesBankAccount.Deposit);
+            BankAccount bankAccount2 = new BankAccount(3000, BankAccount.TypesBankAccount.Deposit);
+            Console.WriteLine($"Сравнение на равенство {bankAccount1 == bankAccount2}");
+            Console.WriteLine($"Сравнение на не равенство {bankAccount1 != bankAccount2}");
+            Console.WriteLine($"Сравнение на Equals {bankAccount1.Equals(bankAccount2)}");
+            Console.WriteLine($"GetHashCode для первого счета {bankAccount1.GetHashCode()} для втрого {bankAccount2.GetHashCode()}");
+            Console.WriteLine($"Представлнение счета {bankAccount1.ToString()}");
+            /*
+             * Создать класс Figure для работы с геометрическими фигурами. 
+             * В качестве полей класса задаются цвет фигуры, состояние «видимое/невидимое». 
+             * Реализовать операции: передвижение геометрической фигуры по горизонтали, по вертикали, изменение цвета, опрос состояния (видимый/невидимый). 
+             * Метод вывода на экран должен выводить состояние всех полей объекта. 
+             * Создать класс Point (точка) как потомок геометрической фигуры. 
+             * Создать класс Circle (окружность) как потомок точки. 
+             * В класс Circle добавить метод, который вычисляет площадь окружности. 
+             * Создать класс Rectangle (прямоугольник) как потомок точки, реализовать метод вычисления площади прямоугольника. 
+             * Точка, окружность, прямоугольник должны поддерживать методы передвижения по горизонтали и вертикали, изменения цвета.
+             */
+            Console.WriteLine("Задание 2");
+            Point point = new Point(10, 15, FigureColourEnum.blue, true);
+            point.MoveX(20);
+            point.MoveY(-5);
+            point.ChangeColour(FigureColourEnum.green);
+            Console.WriteLine("Параметры точки");
+            point.PrintParameters();
+
+            Circle circle = new Circle(15, 15, FigureColourEnum.blue, true, 5);
+            circle.MoveX(5);
+            circle.MoveY(-5);
+            circle.ChangeColour(FigureColourEnum.red);
+            Console.WriteLine("Параметры круга");
+            circle.PrintParameters();
+
+            Rectangle rectangle = new Rectangle(25, 15, FigureColourEnum.green, true, 6, 7);
+            rectangle.MoveX(-5);
+            rectangle.MoveY(5);
+            rectangle.ChangeColour(FigureColourEnum.blue);
+            Console.WriteLine("Параметры прямоугольника");
+            rectangle.PrintParameters();
+        }
+    }
+}
